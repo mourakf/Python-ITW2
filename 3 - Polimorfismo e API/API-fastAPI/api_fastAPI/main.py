d2869ec5443fa6351fc696f7ad76aac85db14383
Karina Moura <57039290+kfmr@users.noreply.github.com>
2022-08-13 19:25:42 -0300

    conteudo aula 4

--- 3 - Polimorfismo e API/API-fastAPI/api_fastAPI/main.py
+++ 3 - Polimorfismo e API/API-fastAPI/api_fastAPI/main.py
@@ -1,13 +1,28 @@
-from fastapi import FastAPI
-from typing import Union
+from fastapi import FastAPI, Header
+from typing import Union, Optional
+# modelo
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    id: int
+    dsc: str
+    value: float
+
+
 app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+async def read_root(user_agent: Optional[str] = Header(None)):
+    return user_agent
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: int, p: bool, q: Union[str, None] = None):
+    return {"item_id": item_id, "p": p, "q": q}
+
+
+@app.post("/items")
+async def add_item(novo_item: Item):
+    return novo_item
